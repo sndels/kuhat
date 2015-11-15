9cf9179f62ef7cbb643f894d086f272dbd3c121f
Antti-Jussi Inkinen <antti-jussi.inkinen@aalto.fi>
2015-11-15 03:51:05 +0200

    Character is flipped to face moving direction.

--- src/character.hpp
+++ src/character.hpp
@@ -13,6 +13,7 @@ public:
         _sprite.setTexture(_texture);
         _sprite.setPosition(820,350);
         _sprite.setScale(0.5,0.5);
+        _isFlipped = false; // Characters are drawn facing left.
     }
 
     sf::Sprite getSprite() {
@@ -20,11 +21,40 @@ public:
     }
 
     void move(float x, float y) {
+        // Change the sprite to face the x-axis moving direction.
+        if ( x<0 && _isFlipped == true ) flip();
+        else if ( x>0 && _isFlipped == false) flip();
         _sprite.move(x,y);
     }
+
+    bool isSpriteFlipped() {
+        return _isFlipped;
+    }
+
+    /**
+     * Flips the character horizontally.
+     *
+     * NOTE: Currently flipping is done by reversing the sprites texture. Which
+     * means that the same texture can't be used for many sprites to save memory,
+     * because they will all be flipped.
+     */
+    void flip() {
+        if (!_isFlipped) {
+            // Flip the texture on x-axis
+            _sprite.setTextureRect(sf::IntRect(_sprite.getTextureRect().width, 0, -(_sprite.getTextureRect().width), _sprite.getTextureRect().height));
+        }
+        else {
+            // Use normal texture
+            _sprite.setTexture(_texture, true);
+        }
+        // Toggle flipped status
+        _isFlipped ^= 1;
+    }
+
 private:
     sf::Texture _texture;
     sf::Sprite _sprite;
+    bool _isFlipped;
 };
 
 #endif
