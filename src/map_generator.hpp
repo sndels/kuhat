852054c443aecdf3bd7c523cc24031ce56b6956b
Santeri Salmijärvi <santeri.salmijarvi@aalto.fi>
2015-11-21 14:02:42 +0200

    Update comments

--- src/map_generator.hpp
+++ src/map_generator.hpp
@@ -14,11 +14,19 @@
 #define MAPPATH "resource/map_bitmask.png"
 
 /**
- * Gives randomized coordinates for map drawing
+ * Returns randomized heights
  *
- * Uses midpoint displacement at highest even division for the given width
+ * Uses midpoint displacement with given parameters and xorshift to generate
+ * randomized height values for the desired map width
+ *
+ * @param seed         seed for the prng
+ * @param width        map width
+ * @param height       max height
+ * @param displacement max deviation for the midpoints
+ * @param roughness    coefficient for change in displacement as step gets smaller
+ * @return             generated height values
  */
-std::vector<double> getHeights(std::string const &seed, int width, int const &height,
+std::vector<double> getHeights(std::string const& seed, int width, int const& height,
                             int displacement, float roughness) {
     Xorshift xors(seed);
     std::vector<double> heights;
@@ -37,6 +45,15 @@ std::vector<double> getHeights(std::string const &seed, int width, int const &he
     return heights;
 }
 
+
+/**
+ * Map generator -function
+ *
+ * Generates a b/w -mask from seed and saves it to a .png -file
+ *
+ * @param seed seed for the prng
+ * @return     zero if generated succesfully, -1 for error
+ */
 int getMap(std::string const& seed) {
     //Get randomized height coordinates
     std::vector<double> heights = getHeights(seed, MAPWIDTH, MAPHEIGHT,
@@ -53,16 +70,18 @@ int getMap(std::string const& seed) {
     sf::Image mapMask;
     mapMask.create(MAPWIDTH, MAPHEIGHT, sf::Color::Black);
     double height;
+    //Loop over the map width and draw the mask
     for (auto i = 0; i < MAPWIDTH; ++i) {
         height = s(i);
         if (!(height < 0) && !(height > MAPHEIGHT)) {
             //Draw border pixel
             mapMask.setPixel(i,height, sf::Color::White);
-            //Fill "under" border
+            //Fill under border
             for (auto j = height; j < MAPHEIGHT; ++j)
                 mapMask.setPixel(i,j, sf::Color::White);
         }
     }
+    //Save the mask to set file
     if (!mapMask.saveToFile(MAPPATH))
         return -1;
     return 0;
