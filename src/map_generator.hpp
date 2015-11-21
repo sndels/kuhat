1a123d2bbacfb8d24f85317f136349a00fda719d
Santeri Salmijärvi <santeri.salmijarvi@aalto.fi>
2015-11-21 13:14:20 +0200

    Add spline-lib and drawing map border to file

--- src/map_generator.hpp
+++ src/map_generator.hpp
@@ -1,23 +1,31 @@
 #ifndef MAP_GENERATION_H
 #define MAP_GENERATION_H
 
+#include <SFML/Graphics.hpp>
 #include <vector>
 #include <iostream>
+#include "../resource/libs/spline.hpp"
 #include "xorshift.hpp"
+
+#define MAPWIDTH 1280
+#define MAPHEIGHT 720
+#define DISPLACEMENT 250
+#define ROUGHNESS 0.5
+#define MAPPATH "resource/map_bitmask.png"
 
 /**
  * Gives randomized coordinates for map drawing
  *
  * Uses midpoint displacement at highest even division for the given width
  */
-std::vector<int> getPoints(std::string const &seed, int width, int const &height,
+std::vector<double> getHeights(std::string const &seed, int width, int const &height,
                             int displacement, float roughness) {
     Xorshift xors(seed);
-    std::vector<int> heights;
+    std::vector<double> heights;
     //Get start and end coordinates
     heights.push_back(height/2 + xors.random() * displacement);
     heights.push_back(height/2 + xors.random() * displacement);
-    //Make sure the division is even
+    //Make sure the division is even and displacement big enough to matter
     while ((width % 2 == 0) && (displacement > 10)) {
         width /= 2;
         //Midpoint displacement. Assigning i from insert to prevent invalidating
@@ -29,6 +37,31 @@ std::vector<int> getPoints(std::string const &seed, int width, int const &height
     return heights;
 }
 
-
+int getMap(std::string const& seed) {
+    //Get randomized height coordinates
+    std::vector<double> heights = getHeights(seed, MAPWIDTH, MAPHEIGHT,
+                                         DISPLACEMENT, ROUGHNESS);
+    //Get amount of heights generated
+    int steps = heights.size();
+    //Create x-coordinates to pair with heights
+    std::vector<double> xCoords;
+    for (int i = 0; i <= MAPWIDTH; i += MAPWIDTH / (steps - 1))
+        xCoords.push_back(i);
+    //Set up the spline-lib interpolation
+    tk::spline s;
+    s.set_points(xCoords, heights);
+    sf::Image mapMask;
+    mapMask.create(MAPWIDTH, MAPHEIGHT, sf::Color::Black);
+    double height;
+    for (auto i = 0; i < MAPWIDTH; ++i) {
+        //Flip the height as coordinates originate from top left
+        height = s(i);
+        if (!(height < 0) && !(height > MAPHEIGHT))
+            mapMask.setPixel(i,height, sf::Color::White);
+    }
+    if (!mapMask.saveToFile(MAPPATH))
+        return -1;
+    return 0;
+}
 
 #endif
--- src/test.cpp
+++ src/test.cpp
@@ -3,37 +3,21 @@
 
 #include "map_generator.hpp"
 
-#define MAPWIDTH 1280
-#define MAPHEIGHT 720
-#define SEEDPHRASE "Random seeasdfasddphrase"
-#define DISPLACEMENT 300
-#define ROUGHNESS 0.57
+#define SEEDPHRASE "Random seedphrase"
 
 int main() {
-    sf::RenderWindow window(sf::VideoMode(MAPWIDTH, MAPHEIGHT), "SFML window");
-    window.setVerticalSyncEnabled(true);
-    // window.setKeyRepeatEnabled(false);
-    std::vector<int> heights = getPoints(SEEDPHRASE, MAPWIDTH, MAPHEIGHT,
-                                         DISPLACEMENT, ROUGHNESS);
-    for (auto i : heights)
-        std::cout << i << std::endl;
-    int steps = heights.size();
-    std::vector<int> xCoords;
-    for (int i = 0; i <= MAPWIDTH; i += MAPWIDTH / (steps - 1))
-        xCoords.push_back(i);
-    sf::VertexArray lines(sf::LinesStrip, steps);
-    for (int i = 0; i < steps; ++i)
-        lines[i].position = sf::Vector2f(xCoords[i], heights[i]);
-    while (window.isOpen()) {
-        sf::Event event;
-        while (window.pollEvent(event)) {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
-        window.clear(sf::Color::Black);
-        window.draw(lines);
-        window.display();
-    }
-    window.close();
+    // sf::RenderWindow window(sf::VideoMode(MAPWIDTH, MAPHEIGHT), "SFML window");
+    // window.setVerticalSyncEnabled(true);
+    getMap(SEEDPHRASE);
+    // while (window.isOpen()) {
+    //     sf::Event event;
+    //     while (window.pollEvent(event)) {
+    //         if (event.type == sf::Event::Closed)
+    //             window.close();
+    //     }
+    //     window.clear(sf::Color::Black);
+    //     window.display();
+    // }
+    // window.close();
     return 0;
 }
