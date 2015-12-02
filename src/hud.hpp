57c2a95f616279b3bf3ff256f453a6c5d1b05c88
Juuso Mikkonen <juuso.mikkonen@aalto.fi>
2015-12-02 17:58:10 +0200

    Improved wind indicator graphics

--- src/hud.hpp
+++ src/hud.hpp
@@ -14,13 +14,15 @@ public:
         _spritePower.setScale(0.5,0.5);
 
         _spriteWind.setTexture(_gradient);
-        _spriteWind.setPosition(600,100);
+        _spriteWind.setPosition(650,100);
         _spriteWind.setTextureRect(sf::IntRect(0,0,100,100));
         _spriteWind.setScale(0.5,0.5);
-        _r.setSize(sf::Vector2f(150, 50));
-        _r.setPosition(600, 100);
+        _r.setSize(sf::Vector2f(300, 50));
+        _r.setPosition(500, 100);
         _r.setOutlineThickness(2);
         _r.setOutlineColor(sf::Color::Black);
+
+        _rotated = false;
     }
 
     void setState(float tCharge) {
@@ -28,6 +30,11 @@ public:
     }
     void setWind(int wind) {
         _spriteWind.setTextureRect(sf::IntRect(0,0, 3 * wind, _spriteWind.getTextureRect().height));
+        if ((wind < 0 && !_rotated) || (wind > 0 && _rotated)) {
+            _spriteWind.scale(-1, 1);
+            _rotated = !_rotated;
+        }
+
     }
 
     void drawPower(sf::RenderWindow &window) const {
@@ -42,6 +49,7 @@ private:
     sf::Sprite _spritePower;
     sf::Sprite _spriteWind;
     sf::RectangleShape _r;
+    bool _rotated;
 };
 
 #endif
