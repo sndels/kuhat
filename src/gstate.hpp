581ca18893caf53543df5efec69d409fd33963b7
Antti-Jussi Inkinen <antti-jussi.inkinen@aalto.fi>
2015-12-07 00:12:36 +0200

    Adds pause and resume functions.

--- src/gstate.hpp
+++ src/gstate.hpp
@@ -11,6 +11,10 @@ public:
     GState() { };
     virtual ~GState() { };
 
+    // Functions to pause and resume the gamestate while changing states.
+    virtual void pause() = 0;
+    virtual void resume() = 0;
+
     virtual void handleEvents(Game& game) = 0;
     virtual void update(Game& game) = 0;
     virtual void draw(Game& game) = 0;
