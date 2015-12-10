579eef1c0e62238442b5a246f80957c9b0ad6fd3
Antti-Jussi Inkinen <antti-jussi.inkinen@aalto.fi>
2015-12-10 20:36:57 +0200

    Settings menu now draws all settings on screen.

--- src/settingsmenu.hpp
+++ src/settingsmenu.hpp
@@ -5,6 +5,8 @@
 #include "../resource/libs/minIni/minIni.h"
 
 #include <iostream> // cout
+#include <map>
+#include <vector>
 #include "gstate.hpp"
 
 class SettingsMenu : public GState
@@ -12,22 +14,67 @@ class SettingsMenu : public GState
 public:
 
     SettingsMenu(Game& game) {
+        sf::Text text;
+
+        // Read settings
         _resolution.x = _settings.getl("", "resolution.x", 1280);
         _resolution.y = _settings.getl("", "resolution.y", 720);
 
-        // Slogan
+        // Font for all text
         if (!_aileron_bold_italic.loadFromFile("resource/fonts/aileron/Aileron-BoldItalic.otf") ) {
             std::cout << "Slogan font failed to load from file." << std::endl;
         }
-        _title.setFont(_aileron_bold_italic);
-        _title.setCharacterSize(50);
-        _title.setColor(sf::Color::Black);
-        _title.setString(_settings.gets("", "resolution.x", "1280"));
-        _title.setPosition(_resolution.x/2 - _title.getGlobalBounds().width/2, 100);
-        _options.push_back(std::make_shared<sf::Text>(_title));
-        _title.setString("jee");
-        _title.move(0,100);
-        _options.push_back(std::make_shared<sf::Text>(_title));
+        text.setFont(_aileron_bold_italic);
+
+        // Title
+        text.setCharacterSize(_resolution.y * 0.07 );
+        text.setColor(sf::Color::Black);
+        text.setString("Settings:");
+        text.setPosition(_resolution.x/2 - text.getGlobalBounds().width/2, _resolution.y * 0.10);
+        _options.push_back(std::make_shared<sf::Text>(text));
+        // Option names
+        std::string name;
+        name = "Resolution X: ";
+        text.setString(name);
+        text.setPosition(_resolution.y * 0.07, _resolution.y * 0.28);
+        _options.push_back(std::make_shared<sf::Text>(text));
+        name = "Resolution Y: ";
+        text.setString(name);
+        text.move(0, _resolution.y * 0.14);
+        _options.push_back(std::make_shared<sf::Text>(text));
+        name = "Number of Players: ";
+        text.setString(name);
+        text.move(0, _resolution.y * 0.14);
+        _options.push_back(std::make_shared<sf::Text>(text));
+        name = "Number of Characters: ";
+        text.setString(name);
+        text.move(0, _resolution.y * 0.14);
+        _options.push_back(std::make_shared<sf::Text>(text));
+        name = "Character Speed: ";
+        text.setString(name);
+        text.move(0, _resolution.y * 0.14);
+        _options.push_back(std::make_shared<sf::Text>(text));
+        // Option values
+        name = "resolution.x";
+        text.setString(std::to_string(_resolution.x));
+        text.setPosition(_resolution.x * 0.7, _resolution.y * 0.28);
+        _values.insert(std::pair<std::string, std::shared_ptr<sf::Text>>(name, std::make_shared<sf::Text>(text)));
+        name = "resolution.y";
+        text.setString(std::to_string(_resolution.y));
+        text.move(0, _resolution.y * 0.14);
+        _values.insert(std::pair<std::string, std::shared_ptr<sf::Text>>(name, std::make_shared<sf::Text>(text)));
+        name = "numPlayers";
+        text.setString(std::to_string(_settings.getl("", name, 2)));
+        text.move(0, _resolution.y * 0.14);
+        _values.insert(std::pair<std::string, std::shared_ptr<sf::Text>>(name, std::make_shared<sf::Text>(text)));
+        name = "numChars";
+        text.setString(std::to_string(_settings.getl("", name, 4)));
+        text.move(0, _resolution.y * 0.14);
+        _values.insert(std::pair<std::string, std::shared_ptr<sf::Text>>(name, std::make_shared<sf::Text>(text)));
+        name = "charSpeed";
+        text.setString(std::to_string(_settings.getf("", name, 0.1)));
+        text.move(0, _resolution.y * 0.14);
+        _values.insert(std::pair<std::string, std::shared_ptr<sf::Text>>(name, std::make_shared<sf::Text>(text)));
 
     }
 
@@ -68,8 +115,8 @@ public:
      */
     void draw(sf::RenderWindow& window) {
         window.clear(sf::Color::White);
-        window.draw(_logoSprite);
         for (auto o : _options) window.draw(*o);
+        for (auto v : _values) window.draw(*(v.second));
     }
 
 private:
@@ -77,10 +124,9 @@ private:
     minIni _settings = minIni("settings.ini");
     sf::Vector2i _resolution;
 
+    std::shared_ptr<sf::Text> _title;
     std::vector<std::shared_ptr<sf::Text>> _options;
-    sf::Sprite _logoSprite;
-    sf::Texture _logoTexture;
-    sf::Text _title;
+    std::map<std::string, std::shared_ptr<sf::Text>> _values;
     sf::Font _aileron_bold_italic;
 };
 
